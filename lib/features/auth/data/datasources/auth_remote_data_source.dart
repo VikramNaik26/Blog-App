e537b952d27b331baa52492d3e31fc6958d522af
Vikram Naik <26vikramnaik@gmail.com>
2024-08-06 18:34:57 +0530

    feat(data): create auth datasource

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,50 @@
+import 'package:blog_app/core/error/exception.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract interface class AuthRemoteDataSource {
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
+  final SupabaseClient supabaseClient;
+
+  AuthRemoteDataSourceImpl({required this.supabaseClient});
+  @override
+  Future<String> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await supabaseClient.auth.signUp(
+        password: password,
+        email: email,
+        data: {'name': name},
+      );
+
+      if (response.user == null) {
+        throw const ServerException('User is null');
+      }
+      return response.user!.id;
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+}
