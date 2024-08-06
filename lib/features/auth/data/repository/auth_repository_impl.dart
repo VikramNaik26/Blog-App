dae93d5673c165e2e2dd4c30f2a3eaa6e124c21a
Vikram Naik <26vikramnaik@gmail.com>
2024-08-06 18:46:20 +0530

    feat(data): create data repository impl domain repo

---
+++ lib/features/auth/data/repository/auth_repository_impl.dart
@@ -0,0 +1,39 @@
+import 'package:blog_app/core/error/exception.dart';
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDataSource authRemoteDataSource;
+
+  const AuthRepositoryImpl(this.authRemoteDataSource);
+
+  @override
+  Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  }) {
+    // TODO: implement loginWithEmailPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final userId = await authRemoteDataSource.signUpWithEmailPassword(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      return right(userId);
+    } on ServerException catch (e) {
+      return left(Failure(e.message));
+    }
+  }
+}
