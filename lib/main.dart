939b41204882a2ad8b4382b0e9cd7d70b8cf2877
Vikram Naik <26vikramnaik@gmail.com>
2024-08-06 18:03:00 +0530

    feat(initialize): set up supabase database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final supabase = Supabase.initialize(
+    url: AppSecret.supabaseUrl,
+    anonKey: AppSecret.supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
