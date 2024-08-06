07054bfde05dd5bc76626452f49e8cb3f94b0258
Vikram Naik <26vikramnaik@gmail.com>
2024-08-06 22:17:35 +0530

    feat(domain): create signup usecase

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:fpdart/fpdart.dart';
+
+import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/usecase/usecase.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  UserSignUp(this.authRepository);
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmailPassword(
+      name: params.name,
+      email: params.email,
+      password: params.email,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String email;
+  final String password;
+  final String name;
+
+  UserSignUpParams({
+    required this.email,
+    required this.password,
+    required this.name,
+  });
+}
