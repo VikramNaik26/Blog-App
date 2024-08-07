1efa8cb084789800dd6c1efe6c3fce4243dc6091
Vikram Naik <26vikramnaik@gmail.com>
2024-08-08 01:11:11 +0530

    feat(blog): add function to select the image from gallery

--- lib/features/blog/presentation/pages/add_new_blog_page.dart
+++ lib/features/blog/presentation/pages/add_new_blog_page.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:blog_app/core/theme/app_pallete.dart';
+import 'package:blog_app/core/utils/pick_image.dart';
 import 'package:blog_app/features/blog/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class AddNewBlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(
@@ -17,6 +21,17 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
   final TextEditingController titleController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
   List<String> selectedTopics = [];
+  File? image;
+
+  void selectedImage() async {
+    final pickedImage = await pickImage();
+
+    if (pickedImage != null) {
+      setState(() {
+        image = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -41,33 +56,51 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              DottedBorder(
-                color: AppPallete.borderColor,
-                dashPattern: const [10, 4],
-                radius: const Radius.circular(10),
-                borderType: BorderType.RRect,
-                strokeCap: StrokeCap.round,
-                child: const SizedBox(
-                  height: 150,
-                  width: double.infinity,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.folder_open,
-                        size: 40,
+              image != null
+                  ? GestureDetector(
+                      onTap: selectedImage,
+                      child: SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                      SizedBox(
-                        height: 15,
+                    )
+                  : GestureDetector(
+                      onTap: selectedImage,
+                      child: DottedBorder(
+                        color: AppPallete.borderColor,
+                        dashPattern: const [10, 4],
+                        radius: const Radius.circular(10),
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        child: const SizedBox(
+                          height: 150,
+                          width: double.infinity,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "Select your image",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      Text(
-                        "Select your image",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
               const SizedBox(height: 20),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
