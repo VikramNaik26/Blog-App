9635af0628e923bb6fb41d46d440ae9c23e0d0d5
Vikram Naik <26vikramnaik@gmail.com>
2024-08-09 00:09:21 +0530

    feat(file): add file upload using supabase storage

--- lib/features/blog/data/datasources/blog_remote_data_source.dart
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:blog_app/core/error/exception.dart';
 import 'package:blog_app/features/blog/data/models/blog_model.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class BlogRemoteDataSource {
   Future<BlogModel> uploadBlog(BlogModel blog);
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  });
 }
 
 class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
@@ -18,6 +24,20 @@ class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
           await supabaseClient.from('blogs').insert(blog.toJson()).select();
 
       return BlogModel.fromJson(blogData.first);
+    } catch (e) {
+      throw ServerException(e.toString());
+    }
+  }
+
+  @override
+  Future<String> uploadBlogImage({
+    required File image,
+    required BlogModel blog,
+  }) async {
+    try {
+      await supabaseClient.storage.from('blog_imagesA').upload(blog.id, image);
+
+      return supabaseClient.storage.from('blog_images').getPublicUrl(blog.id);
     } catch (e) {
       throw ServerException(e.toString());
     }
